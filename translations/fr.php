1e49c8cffd70d437005c839c5c460dd2a2d9cd27
Jerome Danthinne <jerome@grincheux.be>
2015-03-19 10:45:47 +0100

    Traductions françaises

---
+++ translations/fr.php
@@ -0,0 +1,22 @@
+<?php
+
+global $_MODULE;
+$_MODULE = array();
+$_MODULE['<{advancedfeaturesvalues}prestashop>advancedfeaturesvalues_52444beab2c8168a0b6ae77b614641eb'] = 'Valeurs de caractéristiques avancées';
+$_MODULE['<{advancedfeaturesvalues}prestashop>advancedfeaturesvalues_fa4c4a634ea71aa62d2349969bcdbb6b'] = 'Permet de sélectionner plusieurs valeurs de caractéristiques, ainsi que de les ordonner.';
+$_MODULE['<{advancedfeaturesvalues}prestashop>advancedfeaturesvalues_876f23178c29dc2552c0b48bf23cd9bd'] = 'Êtes-vous sûr de vouloir désinstaller ce module?';
+$_MODULE['<{advancedfeaturesvalues}prestashop>features_16b71e8aeabca389b55423f00bd17e33'] = 'Attribuer des caractéristiques à ce produit';
+$_MODULE['<{advancedfeaturesvalues}prestashop>features_e05477ed7830eede8e2f6473a5fb1873'] = 'Vous pouvez préciser une valeur pour chaque caractéristique pertinente concernant ce produit ; les champs vides ne seront pas affichés.';
+$_MODULE['<{advancedfeaturesvalues}prestashop>features_1ebd268cbd989b7119c29d63cd622222'] = 'Vous pouvez définir une valeur spécifique, ou choisir parmi les valeurs existantes prédéfinies que vous avez ajoutées précédemment.';
+$_MODULE['<{advancedfeaturesvalues}prestashop>features_21021ea0e52be8e9c599f4dff41e5be0'] = 'Caractéristique';
+$_MODULE['<{advancedfeaturesvalues}prestashop>features_8bcdd31a1ab28f0db60efb2087c3f235'] = 'Valeur prédéfinie';
+$_MODULE['<{advancedfeaturesvalues}prestashop>features_e81c4e4f2b7b93b481e13a8553c2ae1b'] = 'ou';
+$_MODULE['<{advancedfeaturesvalues}prestashop>features_12085f07b6313b69c63a625d8e23f706'] = 'Valeur personnalisée';
+$_MODULE['<{advancedfeaturesvalues}prestashop>features_382b0f5185773fa0f67a8ed8056c7759'] = 'N/D';
+$_MODULE['<{advancedfeaturesvalues}prestashop>features_544baae42433c186ff222736db985d7d'] = 'Ajouter une valeur prédéfinie dans un premier temps';
+$_MODULE['<{advancedfeaturesvalues}prestashop>features_5fb1f955b45e38e31789286a1790398d'] = 'TOUS';
+$_MODULE['<{advancedfeaturesvalues}prestashop>features_d61248df0ca451d57196a4b4cdb35e1b'] = 'Il n\'y a pas de caractéristiques définies';
+$_MODULE['<{advancedfeaturesvalues}prestashop>features_d6ae681fcd58f1e1936ca97da36528ff'] = 'Ajouter une nouvel caractéristique';
+$_MODULE['<{advancedfeaturesvalues}prestashop>features_ea4788705e6873b424c65e91c2846b19'] = 'Annuler';
+$_MODULE['<{advancedfeaturesvalues}prestashop>features_c9cc8cce247e49bae79f15173ce97354'] = 'Enregistrer';
+$_MODULE['<{advancedfeaturesvalues}prestashop>features_9ea67be453eaccf020697b4654fc021a'] = 'Enregistrer et rester';
