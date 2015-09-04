0d3f7e6ae9af382ddd1720b9c6413a4c8bc80ecd
Jerome Danthinne <jerome@grincheux.be>
2015-09-04 14:04:49 +0200

    Set the group_concat_max_len in a separate query.

--- override/classes/Product.php
+++ override/classes/Product.php
@@ -47,8 +47,8 @@ class Product extends ProductCore
 			return array();
 		if (!array_key_exists($id_product.'-'.$id_lang, self::$_frontFeaturesCache))
 		{
+			Db::getInstance(_PS_USE_SQL_SLAVE_)->executeS('SET @@group_concat_max_len = 4096');
 			self::$_frontFeaturesCache[$id_product.'-'.$id_lang] = Db::getInstance(_PS_USE_SQL_SLAVE_)->executeS('
-				SET @@group_concat_max_len = 4096;
 				SELECT name, GROUP_CONCAT(value ORDER BY fv.position SEPARATOR ", ") AS value, pf.id_feature
 				FROM '._DB_PREFIX_.'feature_product pf
 				LEFT JOIN '._DB_PREFIX_.'feature_lang fl ON (fl.id_feature = pf.id_feature AND fl.id_lang = '.(int)$id_lang.')
